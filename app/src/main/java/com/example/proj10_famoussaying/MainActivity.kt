346d2a3196fb59fc5927265f0cb2478e40653f4f
ksj <koreatlwls@gmail.com>
2021-12-23 21:27:32 +0900

    finished
    finished

--- app/src/main/java/com/example/proj10_famoussaying/MainActivity.kt
+++ app/src/main/java/com/example/proj10_famoussaying/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.proj10_famoussaying
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,10 +18,15 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.viewPager)
     }
 
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
         initData()
     }
 
@@ -30,11 +38,28 @@ class MainActivity : AppCompatActivity() {
             }
         )
         remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
             if (it.isSuccessful) {
                 val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
                 val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
 
                 displayQuotesPager(quotes, isNameRevealed)
+            }
+        }
+    }
+
+    private fun initViews() {
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1F -> {
+                    page.alpha = 0F
+                }
+                position == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha = 1F - 2 * position.absoluteValue
+                }
             }
         }
     }
@@ -57,10 +82,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun displayQuotesPager(quotes:List<Quote>, isNameRevealed: Boolean){
-        viewPager.adapter = QuotesPagerAdapter(
+    private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
+        val adapter = QuotesPagerAdapter(
             quotes = quotes,
             isNameRevealed = isNameRevealed
         )
+        viewPager.adapter = adapter
+        viewPager.setCurrentItem(adapter.itemCount / 2, false)
     }
 }
